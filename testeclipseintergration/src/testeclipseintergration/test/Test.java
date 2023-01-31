5e0a5d365958c6bb48a7dc08402521448e79f8c8
Ihsan Redhauddin <19FTT1094@student.pb.edu.bn>
2023-01-31 15:51:38 +0800

    Commited by Ihsan

--- testeclipseintergration/src/testeclipseintergration/test/Test.java
+++ testeclipseintergration/src/testeclipseintergration/test/Test.java
@@ -2,7 +2,7 @@ package testeclipseintergration.test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Test Git");
+		System.out.println("Test Git huehuehuehuehue");
 		
 		ClassA classA = new ClassA("ABCDEF");
 		
