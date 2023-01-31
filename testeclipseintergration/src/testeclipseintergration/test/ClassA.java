dbdef9bce7196f5d2035893b283c2b26e5359513
muhdm <20FTT1416@student.pb.edu.bn>
2023-01-31 15:26:58 +0800

    modified file

---
+++ testeclipseintergration/src/testeclipseintergration/test/ClassA.java
@@ -0,0 +1,18 @@
+package testeclipseintergration.test;
+
+public class ClassA {
+	
+	private String abc;
+	
+	public ClassA(String abc) {
+		this.abc = abc;
+	}
+	
+	public String getAbc() {
+		return abc;
+	}
+	
+	public void setAbc(String abc) {
+		this.abc = abc;
+	}
+}
--- testeclipseintergration/src/testeclipseintergration/test/Test.java
+++ testeclipseintergration/src/testeclipseintergration/test/Test.java
@@ -3,6 +3,10 @@ package testeclipseintergration.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test Git");
+		
+		ClassA classA = new ClassA("ABCDEF");
+		
+		System.out.println(classA.getAbc());
 	}
 
 }
